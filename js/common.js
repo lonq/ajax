3dfdd5c6037c3de4919356c988a50ade4788fdc6
lonq <lonq@163.com>
2018-12-16 23:50:07 +0800

    ajax

--- js/common.js
+++ js/common.js
@@ -1,17 +1,47 @@
 var selfUrl = 'http://' + window.location.host + '/ajax'; // 本站网址
 $.fn.cookie('prevLink', document.referrer); // 来路网址
 
-$(function () {
+// footer菜单
+function getFooter(pageName) {
+    var html = '<ul class="footer affix affix-bottom tabs tabs-vertical text-center">' +
+    '<li>' +
+    '<a class="btn btn-clear" href="index.html">' +
+    '<i class="icon"></i><span class="tip">首页</span>' +
+    '</a>' +
+    '</li>' +
+    '<li>' +
+    '<a class="btn btn-clear" href="productList.html">' +
+    '<i class="icon"><span class="badge badge-danger">6</span></i><span class="tip">产品</span>' +
+    '</a>' +
+    '</li>' +
+    '<li>' +
+    '<a class="btn btn-clear" href="articleList.html">' +
+    '<i class="icon"></i><span class="tip">文章</span>' +
+    '</a>' +
+    '</li>' +
+    '<li>' +
+    '<a class="btn btn-clear" href="userCenter.html">' +
+    '<i class="icon"></i><span class="tip">我</span>' +
+    '</a>' +
+    '</li>' +
+    '</ul>';
+    $('#footer').html(html);
+
+    $('ul.footer>li>a').each(function(i) {
+        var _t1 = $(this);
+        var hrefVal = _t1.attr('href');
+        if(pageName == hrefVal) {
+            _t1.addClass('active');
+        }
+    });
     // 禁止底部主菜单激活后的行为
-    $('.footer.tabs-vertical .btn').on('click', function(e){
-        var _t = $(this);
-        if (_t.hasClass('active')){
+    $(document).on('click', 'ul.footer>li>a', function(e){
+        var _t2 = $(this);
+        if (_t2.hasClass('active')){
             e.preventDefault();
         }
     });
-});
-
-
+}
 // 返回来路网址
 function forwardUrl(prevLink) {
     if ($.trim(prevLink) == '') {
