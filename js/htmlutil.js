04cad36fedb86962bb0e123cda72fa89add5ba15
lonq <lonq@163.com>
2019-01-16 15:21:18 +0800

    ajax

--- js/htmlutil.js
+++ js/htmlutil.js
@@ -94,3 +94,17 @@ var HtmlUtil = {
         return '';
     }
 };
+
+// 长按事件
+$.fn.longPress = function (fn) {
+    var timeout = undefined;
+    var $this = this;
+    for (var i = 0; i < $this.length; i++) {
+        $this[i].addEventListener('touchstart', function (event) {
+            timeout = setTimeout(fn, 3800);  //长按时间超过800ms，则执行传入的方法
+        }, false);
+        $this[i].addEventListener('touchend', function (event) {
+            clearTimeout(timeout);  //长按时间少于800ms，不会执行传入的方法
+        }, false);
+    }
+}
