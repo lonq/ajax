ee1723d89c80d799101d492260d1179cbaec0e46
lonq <lonq@163.com>
2019-02-18 16:44:08 +0800

    ajax

--- js/htmlutil.js
+++ js/htmlutil.js
@@ -1,11 +1,11 @@
 var HtmlUtil = {
-    // 对Date的扩展，将 Date 转化为指定格式的String
+    // 1.对Date的扩展，将 Date 转化为指定格式的String
     // 月(M)、日(d)、小时(H)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
     // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
     // 例子：
     // dateFormat("yyyy-MM-dd HH:mm:ss.S", new Date()) ==> 2006-07-02 08:09:04.423
     // dateFormat.Format("yyyy-M-d H:m:s.S", new Date())      ==> 2006-7-2 8:9:4.18
-    dateFormat: function (fmt, date) {
+    dateFormat: function(fmt, date) {
         var o = {
             "M+": date.getMonth() + 1, //月份
             "d+": date.getDate(), //日
@@ -13,28 +13,25 @@ var HtmlUtil = {
             "m+": date.getMinutes(), //分
             "s+": date.getSeconds(), //秒
             "q+": Math.floor((date.getMonth() + 3) / 3), //季度
-            "S": date.getMilliseconds() //毫秒
+            S: date.getMilliseconds() //毫秒
         };
-        if (/(y+)/.test(fmt))
-            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt))
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o) if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
         return fmt;
     },
-    /*1.用浏览器内部转换器实现html转码*/
-    htmlEncode: function (html) {
+    // 2.用浏览器内部转换器实现html转码
+    htmlEncode: function(html) {
         //1.首先动态创建一个容器标签元素，如DIV
         var temp = document.createElement("div");
         //2.然后将要转换的字符串设置为这个元素的innerText(ie支持)或者textContent(火狐，google支持)
-        (temp.textContent != undefined) ? (temp.textContent = html) : (temp.innerText = html);
+        temp.textContent != undefined ? (temp.textContent = html) : (temp.innerText = html);
         //3.最后返回这个元素的innerHTML，即得到经过HTML编码转换的字符串了
         var output = temp.innerHTML;
         temp = null;
         return output;
     },
-    /*2.用浏览器内部转换器实现html解码*/
-    htmlDecode: function (text) {
+    // 3.用浏览器内部转换器实现html解码
+    htmlDecode: function(text) {
         //1.首先动态创建一个容器标签元素，如DIV
         var temp = document.createElement("div");
         //2.然后将要转换的字符串设置为这个元素的innerHTML(ie，火狐，google都支持)
@@ -44,8 +41,8 @@ var HtmlUtil = {
         temp = null;
         return output;
     },
-    /*3.用正则表达式实现html转码*/
-    htmlEncodeByRegExp: function (str) {
+    // 4.用正则表达式实现html转码
+    htmlEncodeByRegExp: function(str) {
         var s = "";
         if (str.length == 0) return "";
         s = str.replace(/&/g, "&amp;");
@@ -56,41 +53,60 @@ var HtmlUtil = {
         s = s.replace(/\"/g, "&quot;");
         return s;
     },
-    /*4.用正则表达式实现html解码*/
-    htmlDecodeByRegExp: function (str) {
+    // 5.用正则表达式实现html解码
+    htmlDecodeByRegExp: function(str) {
         var s = "";
         if (str.length == 0) return "";
         s = str.replace(/&amp;/g, "&");
         s = s.replace(/&lt;/g, "<");
         s = s.replace(/&gt;/g, ">");
         s = s.replace(/&nbsp;/g, " ");
-        s = s.replace(/&#39;/g, "\'");
-        s = s.replace(/&quot;/g, "\"");
+        s = s.replace(/&#39;/g, "'");
+        s = s.replace(/&quot;/g, '"');
         return s;
     },
-    /*4.获取地址栏参数，str:参数名称*/
-    getUrlParms: function (str) {
+    // 6.获取地址栏参数，str:参数名称
+    getUrlParms: function(str) {
         var reg = new RegExp("(^|&)" + str + "=([^&]*)(&|$)");
         var r = window.location.search.substr(1).match(reg);
-        if (r != null)
-            return unescape(r[2]);
+        if (r != null) return unescape(r[2]);
         return null;
     },
-    /*4.读取cookie数组，str:参数名称*/
-    getCookie: function (arr, str) {
+    // 7.读取cookie数组，str:参数名称
+    getCookie: function(arr, str) {
         //'username=abc; password=123456; aaa=123; bbb=4r4er'是一个字符串
         // var arr = document.cookie.split('&');
         if (!arr) return;
-        arr = arr.split('&');
+        arr = arr.split("&");
         var i = 0;
         //arr->['username=abc', 'password=123456', ...]
         for (i = 0; i < arr.length; i++) {
             //arr2->['username', 'abc']
-            var arr2 = arr[i].split('=');
+            var arr2 = arr[i].split("=");
             if (arr2[0] == str) {
                 return arr2[1];
             }
         }
         return null;
+    },
+    // 8.修正定位在底部的元素
+    fixBottom: function($obj) {
+        var winH = $(window).height();
+        var $foot = $(document).find("." + $obj);
+        if ($foot.length < 1) return;
+        if ($foot.position().top + $foot.height() < winH) {
+            $foot.addClass("affix affix-bottom");
+        } else {
+            $foot.removeClass("affix affix-bottom");
+        }
+    },
+    // 9.无阻断消息提示(基于“dialog2-master”插件)
+    msg: function (str) {
+        $(document).dialog({
+            type: 'notice',
+            infoText: str,
+            autoClose: 1500,
+            position: 'bottom'
+        });
     }
-}
+};
